5aff273c1c92406919b69deca236153cd1581030
majharul <majharuljony163@gmail.com>
2022-11-29 16:21:41 +0600

    style product details tab

--- src/pages/product/productSingleNew/includes/SameSellerProducts.js
+++ src/pages/product/productSingleNew/includes/SameSellerProducts.js
@@ -6,7 +6,7 @@ const SameSellerProducts = () => {
       <div className='pl-head'>
         <h4>From The Same Seller</h4>
         <a href='/shop/china/store-abb-b2b-2976843313dd790'>
-          <button class='bt'>View all</button>
+          <button class='btn btn-default px-4 py-2 py-md-3 rounded'>View all</button>
         </a>
       </div>
     </div>
