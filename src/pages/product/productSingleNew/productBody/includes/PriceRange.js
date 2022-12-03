04ff4fabc6079fa8b7c35bf5310b090a449d882d
majharul <majharuljony163@gmail.com>
2022-12-03 18:24:14 +0600

    select quntity range

--- src/pages/product/productSingleNew/productBody/includes/PriceRange.js
+++ src/pages/product/productSingleNew/productBody/includes/PriceRange.js
@@ -1,8 +1,41 @@
 import React from "react";
 
 const PriceRange = (props) => {
-  // console.log("props", props);
+  console.log("props from pricerange", props);
   const { totalQty, bulkPriceQuantity } = props;
+
+  let first = bulkPriceQuantity[0].MinQuantity;
+  let second = bulkPriceQuantity[1].MinQuantity;
+  let third = bulkPriceQuantity[2].MinQuantity;
+
+  // quantity box active deactive
+  const addActiveClass = (MinQuantity, MaxQuantity) => {
+    let a = totalQty;
+    let b = document.querySelectorAll(".range");
+    let i;
+    for (i = 0; i < b.length; i++) {
+      if (a < first) {
+        b[i].classList.remove("rangeActive");
+      } else if (a == first || a < second) {
+        b[1].classList.remove("rangeActive");
+        b[2].classList.remove("rangeActive");
+        b[0].classList.add("rangeActive");
+      } else if (a == second || a < third) {
+        b[0].classList.remove("rangeActive");
+        b[2].classList.remove("rangeActive");
+        b[1].classList.add("rangeActive");
+      } else if (a == third) {
+        b[1].classList.remove("rangeActive");
+        b[2].classList.add("rangeActive");
+      }
+    }
+  };
+
+  // console.log("first", first);
+  // console.log("first", second);
+  // console.log("first", third);
+  // console.log("quantity", totalQty);
+
   return (
     <div className='ranges'>
       {bulkPriceQuantity.map((pqR, index) => {
@@ -12,8 +45,11 @@ const PriceRange = (props) => {
           MinQuantity,
           Price: { Base },
         } = pqR;
+        // console.log("MinQuantity", MinQuantity);
+        addActiveClass(MinQuantity, MaxQuantity);
+
         return (
-          <div className={`range`} key={index}>
+          <div className='range' key={index}>
             <span className='amount'>৳ {Base}</span>
             <div className='piece'>{MinQuantity} or more</div>
           </div>
