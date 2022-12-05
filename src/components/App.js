2430db0f32acf8938e1cd64fdbb072a1e040e33a
majharul <majharuljony163@gmail.com>
2022-12-05 18:22:52 +0600

    fix cart count

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routing from "./Routing";
-
 import HeaderManage from "./header/HeaderManage";
 import Footer from "./footer/Footer";
 import ErrorHandling from "../errorHandler/ErrorHandling";
 import GlobalLoading from "../loader/GlobalLoading";
-import BrowseCategories from "../pages/home/includes/intro/includes/BrowseCategories";
 
 const App = () => {
   return (
