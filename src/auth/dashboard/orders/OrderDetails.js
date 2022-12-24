bf4f4e89ba8aa5f3af2d7a7a7340a4532b27e799
majharul <majharuljony163@gmail.com>
2022-12-24 10:29:35 +0600

    order details table issue

--- src/auth/dashboard/orders/OrderDetails.js
+++ src/auth/dashboard/orders/OrderDetails.js
@@ -1,194 +1,195 @@
-import React, {useEffect, useState} from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
-import {getSetting, goPageTop} from "../../../utils/Helpers";
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+import { getSetting, goPageTop } from "../../../utils/Helpers";
 import Breadcrumb from "../../../pages/breadcrumb/Breadcrumb";
 import ConfigItem from "../../../pages/payment/includes/ConfigItem";
 import PlainItem from "../../../pages/payment/includes/PlainItem";
 import {
-   calculateAirShippingCharge,
-   cartCalculateNeedToPay,
-   cartCheckedProductTotal,
-   CartProductSummary,
-   numberWithCommas
+  calculateAirShippingCharge,
+  cartCalculateNeedToPay,
+  cartCheckedProductTotal,
+  CartProductSummary,
+  numberWithCommas,
 } from "../../../utils/CartHelpers";
-import {Link, withRouter} from "react-router-dom";
-import {connect} from "react-redux";
-import {confirmCustomerOrder} from "../../../store/actions/CartAction";
-import {getOrderDetails} from "../../../utils/Services";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { confirmCustomerOrder } from "../../../store/actions/CartAction";
+import { getOrderDetails } from "../../../utils/Services";
 
+const OrderDetails = (props) => {
+  const { cartConfigured, match, general } = props;
+  const order_id = match.params.id;
+  const [order, setOrder] = useState("");
 
-const OrderDetails = props => {
-   const {cartConfigured, match, general} = props;
-   const order_id = match.params.id;
-   const [order, setOrder] = useState('');
+  const currency = getSetting(general, "currency_icon");
+  const ShippingCharges = getSetting(general, "air_shipping_charges");
+  const summary = CartProductSummary(cartConfigured, ShippingCharges);
+  const advanced = cartCalculateNeedToPay(summary.totalPrice);
+  const dueAmount = advanced;
 
+  const address = !_.isEmpty(order) ? JSON.parse(order.address) : {};
+  const orderItems = !_.isEmpty(order) ? order.order_items : [];
 
-   const currency = getSetting(general, "currency_icon");
-   const ShippingCharges = getSetting(general, "air_shipping_charges");
-   const summary = CartProductSummary(cartConfigured, ShippingCharges);
-   const advanced = cartCalculateNeedToPay(summary.totalPrice);
-   const dueAmount = advanced;
+  useEffect(() => {
+    goPageTop();
+    getOrderDetails(order_id).then((response) => {
+      setOrder(response.order);
+    });
+  }, [order_id]);
 
-   const address = !_.isEmpty(order) ? JSON.parse(order.address) : {};
-   const orderItems = !_.isEmpty(order) ? order.order_items : [];
+  console.log("order", order);
 
-   useEffect(() => {
-      goPageTop();
-      getOrderDetails(order_id)
-         .then(response => {
-            setOrder(response.order);
-         })
-   }, [order_id]);
+  const totalShippingCost = (product) => {
+    const checkItemSubTotal = cartCheckedProductTotal(product);
+    const totalPrice = checkItemSubTotal.totalPrice;
+    const totalWeight = checkItemSubTotal.totalWeight;
+    const DeliveryCost = product.DeliveryCost;
+    const ShippingRate = calculateAirShippingCharge(totalPrice, ShippingCharges);
+    return Number(DeliveryCost) + Number(totalWeight) * Number(ShippingRate);
+  };
 
-   console.log('order', order)
+  const productTotalCost = (product) => {
+    const checkItemSubTotal = cartCheckedProductTotal(product);
+    const totalPrice = checkItemSubTotal.totalPrice;
+    const ShippingCost = totalShippingCost(product);
+    return Number(totalPrice) + Number(ShippingCost);
+  };
 
-   const totalShippingCost = (product) => {
-      const checkItemSubTotal = cartCheckedProductTotal(product);
-      const totalPrice = checkItemSubTotal.totalPrice;
-      const totalWeight = checkItemSubTotal.totalWeight;
-      const DeliveryCost = product.DeliveryCost;
-      const ShippingRate = calculateAirShippingCharge(totalPrice, ShippingCharges);
-      return Number(DeliveryCost) + (Number(totalWeight) * Number(ShippingRate));
-   };
+  return (
+    <main className='main bg-gray'>
+      <Breadcrumb
+        current='Order details'
+        collections={[
+          { name: "Dashboard", url: "dashboard" },
+          { name: "Orders", url: "dashboard/orders" },
+        ]}
+      />
 
-   const productTotalCost = (product) => {
-      const checkItemSubTotal = cartCheckedProductTotal(product);
-      const totalPrice = checkItemSubTotal.totalPrice;
-      const ShippingCost = totalShippingCost(product);
-      return Number(totalPrice) + Number(ShippingCost);
-   };
+      <div className='page-content'>
+        <div className='container'>
+          <div className='row justify-content-center'>
+            <aside className='col-md-9'>
+              <div className='card bg-white'>
+                <div className='card-body pt-5 px-5 py-4'>
+                  <table className='table table-responsive table-borderless' style={{ tableLayout: "fixed" }}>
+                    <thead>
+                      <tr>
+                        <td>
+                          <p className='m-0'>Customer: {order.name}</p>
+                          <p className='m-0'>Email: {order.email}</p>
+                          <p className='m-0'>Phone: {order.phone}</p>
+                          <p className='m-0'>Address: {!_.isEmpty(address) ? address.address : ""}</p>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                          <p className='m-0'>
+                            <b>UNPAID</b>
+                          </p>
+                          <p className='m-0'>
+                            <span className='mr-2'>Method:</span>
+                            {order.pay_method === "bkash_payment" && `Bkash`}
+                            {order.pay_method === "nagad_payment" && `Nagod`}
+                          </p>
+                          <p className='m-0'>
+                            <span className='mr-2'>Wallet:</span>
+                            {order.pay_method === "bkash_payment" && `01811355678`}
+                            {order.pay_method === "nagad_payment" && `01911712769`}
+                          </p>
+                          <p className='m-0'>Ref. : {order.id}</p>
+                        </td>
+                      </tr>
+                    </thead>
+                  </table>
 
-   return (
-      <main className="main bg-gray">
-         <Breadcrumb current="Order details" collections={[
-            {'name': "Dashboard", url: 'dashboard'},
-            {'name': "Orders", url: 'dashboard/orders'}
-         ]}/>
-
-         <div className="page-content">
-            <div className="container">
-               <div className="row justify-content-center">
-                  <aside className="col-md-9">
-                     <div className="card bg-white">
-                        <div className="card-body pt-5 px-5 py-4">
-                           <table className="table table-borderless" style={{tableLayout: "fixed"}}>
-                              <thead>
-                              <tr>
-                                 <td>
-                                    <p className="m-0">Customer: {order.name}</p>
-                                    <p className="m-0">Email: {order.email}</p>
-                                    <p className="m-0">Phone: {order.phone}</p>
-                                    <p className="m-0">Address: {!_.isEmpty(address) ? address.address : ''}</p>
-                                 </td>
-                                 <td></td>
-                                 <td></td>
-                                 <td>
-                                    <p className="m-0"><b>UNPAID</b></p>
-                                    <p className="m-0"><span className="mr-2">Method:</span>
-                                       {order.pay_method === 'bkash_payment' && `Bkash`}
-                                       {order.pay_method === 'nagad_payment' && `Nagod`}
-                                    </p>
-                                    <p className="m-0"><span className="mr-2">Wallet:</span>
-                                       {order.pay_method === 'bkash_payment' && `01811355678`}
-                                       {order.pay_method === 'nagad_payment' && `01911712769`}
-                                    </p>
-                                    <p className="m-0">Ref. : {order.id}</p>
-                                 </td>
-                              </tr>
-                              </thead>
-                           </table>
-
-                           <hr/>
-                           <table className="table table-bordered table-cart">
-                              <thead>
-                              <tr>
-                                 <th> Image</th>
-                                 <th>Title</th>
-                                 <th>Quantity</th>
-                                 <th>Total</th>
-                                 <th>FirstPayment</th>
-                                 <th>DuePayment</th>
-                              </tr>
-                              </thead>
-                              <tbody>
-                              {
-                                 orderItems.length > 0 ?
-                                    orderItems.map((item, index) =>
-                                       <>
-                                          <tr key={index}>
-                                             <td>
-                                                <img src={item.image} style={{width: '100px'}} alt=""/>
-                                             </td>
-                                             <td> {item.name} </td>
-                                             <td> {item.quantity}</td>
-                                             <td> {item.product_value}</td>
-                                             <td> {item.first_payment}</td>
-                                             <td> {item.due_payment}</td>
-                                          </tr>
-                                       </>
-                                    )
-                                    :
-                                    <tr>
-                                       <td colSpan={6} className="text-center bg-lighter">You have no cart!</td>
-                                    </tr>
-
-                              }
-
-                              </tbody>
-                              <tfoot>
-                              <tr>
-                                 <td colSpan={6}>
-                                    <h3 className="border-0 m-0 py-3 summary-title">Cart Total Summary</h3>
-                                 </td>
-                              </tr>
-                              <tr className="summary-total">
-                                 <td colSpan={5} className="text-right">Subtotal:</td>
-                                 <td className="text-right">{`${currency} ${order.amount}`}</td>
-                              </tr>
-                              <tr className="summary-total">
-                                 <td colSpan={5} className="text-right">Need To Pay:</td>
-                                 <td className="text-right">{`${currency} ${order.needToPay}`}</td>
-                              </tr>
-                              <tr className="summary-total">
-                                 <td colSpan={5} className="text-right">Due Amount:</td>
-                                 <td className="text-right">{`${currency} ${order.dueForProducts}`}</td>
-                              </tr>
-
-
-                              </tfoot>
-                           </table>
-
-                        </div>
-                     </div>
-                  </aside>
-               </div>
-               {/* End .row */}
-            </div>
-            {/* End .container */}
-         </div>
-
-
-      </main>
-   );
+                  <hr />
+                  <table className='table table-responsive table-bordered table-cart'>
+                    <thead>
+                      <tr>
+                        <th> Image</th>
+                        <th>Title</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                        <th>FirstPayment</th>
+                        <th>DuePayment</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orderItems.length > 0 ? (
+                        orderItems.map((item, index) => (
+                          <>
+                            <tr key={index}>
+                              <td>
+                                <img src={item.image} style={{ width: "100px" }} alt='' />
+                              </td>
+                              <td> {item.name} </td>
+                              <td> {item.quantity}</td>
+                              <td> {item.product_value}</td>
+                              <td> {item.first_payment}</td>
+                              <td> {item.due_payment}</td>
+                            </tr>
+                          </>
+                        ))
+                      ) : (
+                        <tr>
+                          <td colSpan={6} className='text-center bg-lighter'>
+                            You have no cart!
+                          </td>
+                        </tr>
+                      )}
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <td colSpan={6}>
+                          <h3 className='border-0 m-0 py-3 summary-title'>Cart Total Summary</h3>
+                        </td>
+                      </tr>
+                      <tr className='summary-total'>
+                        <td colSpan={5} className='text-right'>
+                          Subtotal:
+                        </td>
+                        <td className='text-right'>{`${currency} ${order.amount}`}</td>
+                      </tr>
+                      <tr className='summary-total'>
+                        <td colSpan={5} className='text-right'>
+                          Need To Pay:
+                        </td>
+                        <td className='text-right'>{`${currency} ${order.needToPay}`}</td>
+                      </tr>
+                      <tr className='summary-total'>
+                        <td colSpan={5} className='text-right'>
+                          Due Amount:
+                        </td>
+                        <td className='text-right'>{`${currency} ${order.dueForProducts}`}</td>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </div>
+              </div>
+            </aside>
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End .container */}
+      </div>
+    </main>
+  );
 };
 
 OrderDetails.propTypes = {
-   general: PropTypes.object.isRequired,
-   user: PropTypes.object.isRequired,
-   shipping_address: PropTypes.object.isRequired,
-   cartConfigured: PropTypes.array.isRequired,
-   confirmCustomerOrder: PropTypes.func.isRequired,
+  general: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  shipping_address: PropTypes.object.isRequired,
+  cartConfigured: PropTypes.array.isRequired,
+  confirmCustomerOrder: PropTypes.func.isRequired,
 };
 
-
 const mapStateToProps = (state) => ({
-   general: JSON.parse(state.INIT.general),
-   user: state.AUTH.user,
-   cartConfigured: state.CART.configured,
-   shipping_address: state.CART.shipping_address
+  general: JSON.parse(state.INIT.general),
+  user: state.AUTH.user,
+  cartConfigured: state.CART.configured,
+  shipping_address: state.CART.shipping_address,
 });
 
-export default connect(mapStateToProps, {confirmCustomerOrder})(
-   withRouter(OrderDetails)
-);
+export default connect(mapStateToProps, { confirmCustomerOrder })(withRouter(OrderDetails));
