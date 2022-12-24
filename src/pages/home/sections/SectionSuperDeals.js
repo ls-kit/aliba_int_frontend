1e530292b078994cf3fabdc677664b80eb502ab6
majharul <majharuljony163@gmail.com>
2022-12-24 18:20:09 +0600

    working on supper deal

--- src/pages/home/sections/SectionSuperDeals.js
+++ src/pages/home/sections/SectionSuperDeals.js
@@ -5,10 +5,30 @@ import _ from "lodash";
 import { getSuperDeals, loadSectionsProducts } from "../../../utils/Services";
 import ProductSectionSkeleton from "../../../skeleton/productSkeleton/ProductSectionSkeleton";
 import RecentItems from "../includes/Products/recentProduct/includes/RecentItems";
+import Countdown from "react-countdown";
+
+// Random component
+const Completionist = () => <span>Deals closed !</span>;
+
+// Renderer callback with condition
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <span className='bold'>
+        <span className='timeBox'>{hours}</span>: <span className='timeBox'>{minutes}</span>:
+        <span className='timeBox'>{seconds}</span>
+      </span>
+    );
+  }
+};
 
 const SectionSuperDeals = (props) => {
   const { general } = props;
-  console.log("general", general);
+  // console.log("general", general);
 
   const section_super_deals_title = getSetting(general, "section_super_deals_title");
   const section_one_title_image = getSetting(general, "section_one_title_image");
@@ -18,41 +38,49 @@ const SectionSuperDeals = (props) => {
 
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
-
+  const [expiredDate, setExpiredDate] = useState("");
+  const acceptedDate = Date.parse("25 Dec 2022 00:12:00 GMT");
+  console.log("acceptedDate", ` ${expiredDate + " " + "GMT"}`);
   useEffect(() => {
     if (_.isEmpty(products)) {
-      loadSectionsProducts("section_super_deals").then((response) => {
+      getSuperDeals().then((response) => {
         if (!_.isEmpty(response)) {
-          const products = JSON.parse(response.products);
+          const products = response.SuperDealProducts;
+          const timer = response.timer;
           if (!_.isEmpty(products)) {
-            const Content = products.Content;
-            if (!_.isEmpty(Content)) {
-              setProducts(Content);
-            }
+            setProducts(products);
+          }
+          if (!_.isEmpty(timer)) {
+            setExpiredDate(timer);
           }
         }
         setLoading(false);
       });
     }
   }, []);
-  useEffect(() => {
-    getSuperDeals();
-  }, []);
 
   return (
     <div className='container deal-section'>
       <div className='row mt-0 mb-0 mb-md-1 mb-md-1'>
-        <div className='col-6'>
-          <h3 className='title title-sm'>
-            {" "}
-            {section_one_visible_title === "image" ? (
-              <img src={loadAsset(section_one_title_image)} alt='' />
-            ) : (
-              section_super_deals_title || "..."
-            )}
-          </h3>
+        <div className='col-9 d-flex'>
+          <h4 className='bold topTitle'>
+            SUPER <span className='hi-color'>DEALS</span>
+          </h4>
+          <div className='pl-4 flex'>
+            <h4 className='bold topTitle text-muted'>Top Products, Incredible Price !</h4>
+            <div className='pl-2'>
+              <Countdown date={acceptedDate} renderer={renderer} />
+            </div>
+          </div>
         </div>
-        <div className='col-6 text-right'>
+        <div className='col-3 text-right'>
+          {" "}
+          <a href={`/seller/${"vendorId"}?page=2`} className='btn btn-default px-4 py-2 py-md-3 rounded'>
+            View All
+          </a>
+        </div>
+
+        {/* <div className='col-6 text-right'>
           {query_type === "search_query" ? (
             <a href={`/search${query_url}`} className='btn btn-default px-4 py-2 py-md-3 rounded'>
               View All
@@ -62,11 +90,11 @@ const SectionSuperDeals = (props) => {
               View All
             </a>
           )}
-        </div>
+        </div> */}
       </div>
 
       {loading && <ProductSectionSkeleton />}
-      {!loading && products.length > 0 && <RecentItems products={products} />}
+      {/* {!loading && products.length > 0 && <RecentItems products={products} />} */}
     </div>
   );
 };
